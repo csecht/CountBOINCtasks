aaee6a39543b7d7a733b76846e66ad3389fc6009
csecht <spam-me@earthlink.net>
2022-02-03 09:51:24 -0600

    put Python version check into a module

---
+++ COUNTmodules/vcheck.py
@@ -0,0 +1,67 @@
+#!/usr/bin/env python3
+"""
+Simple check of current Python version required for main script.
+
+    Copyright (C) 2020  C. Echt
+
+    This program is free software: you can redistribute it and/or modify
+    it under the terms of the GNU General Public License as published by
+    the Free Software Foundation, either version 3 of the License, or
+    (at your option) any later version.
+
+    This program is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU General Public License for more details.
+
+    You should have received a copy of the GNU General Public License
+    along with this program. If not, see https://www.gnu.org/licenses/.
+"""
+__author__ = 'cecht, BOINC ID: 990821'
+__copyright__ = 'Copyright (C) 2021 C. Echt'
+__license__ = 'GNU General Public License'
+__module_name__ = 'vcheck.py'
+__module_ver__ = '0.1.0'
+__dev_environment__ = "Python 3.8 - 3.9"
+__project_url__ = 'https://github.com/csecht/CountBOINCtasks'
+__maintainer__ = 'cecht'
+__status__ = 'Development Status :: 4 - Beta'
+
+from sys import version_info, exit as sysexit
+
+
+def isversion(req_version: str) -> None:
+    """
+    Check current Python version against version required. Exit program
+    if current version is less than required.
+
+    :param req_version: The required version, example, '3.6'.
+    """
+    ver = tuple(map(int, req_version.split('.')))
+    if version_info < ver:
+        print(f'Sorry, but this program requires Python {req_version} or later.\n'
+              'Current Python version: '
+              f'{version_info.major}.{version_info.minor}\n'
+              'Python downloads are available from https://docs.python.org/')
+    sysexit(0)
+
+
+def about() -> None:
+    """
+    Print basic information about this module.
+    """
+    print(__doc__)
+    print(f'{"Author:".ljust(11)}', __author__)
+    print(f'{"Copyright:".ljust(11)}', __copyright__)
+    print(f'{"License:".ljust(11)}', __license__)
+    print(f'{"Module:".ljust(11)}', __module_name__)
+    print(f'{"Module ver.:".ljust(11)}', __module_ver__)
+    print(f'{"Dev Env:".ljust(11)}', __dev_environment__)
+    print(f'{"URL:".ljust(11)}', __project_url__)
+    print(f'{"Maintainer:".ljust(11)}',  __maintainer__)
+    print(f'{"Status:".ljust(11)}', __status__)
+    sysexit(0)
+
+
+if __name__ == '__main__':
+    about()
